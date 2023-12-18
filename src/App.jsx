d3891c80eeb45df5ae3ecd9fd5ea182045cc5ee0
steelsnake93 <nikolanikotasev@protonmail.com>
2023-12-18 22:51:22 +0100

    Refactor GeneralInfo component, using Material-UI components TextFields

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import DisplayData from './components/DisplayData';
 import Education from './components/Education';
 import Experience from './components/Experience';
-import GeneralInfo from './components/GeneralInfo';
+import { GeneralInfo } from './components/GeneralInfo';
 import { InfoProvider } from './components/InfoContext';
 import '../src/styles/App.css';
 
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,141 +1,70 @@
-import { useState, useContext } from 'react';
-import { InfoContext } from './InfoContext';
-import validator from 'validator';
-import { useButton } from "../Button/Button";
-import '../styles/GeneralInfo.css';
+import { useState } from "react";
+import { Box, TextField } from "@mui/material";
 
-// Define the GeneralInfo component
-export default function GeneralInfo() {
-    const Button = useButton();
-    const { info, setInfo, submittedInfo, setSubmittedInfo } = useContext(InfoContext);
-    const [isEditing, setIsEditing] = useState(true);
-    // State for storing form errors
-    const [errors, setErrors] = useState({});
-    // Handle input changes and update the state
-    const handleChange = (e) => {
-        setInfo({
-            ...info,
-            [e.target.name]: e.target.value
-        });
+export const GeneralInfo = () => {
+    const [info, setInfo] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        portfolioLink: '',
+        address: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        const updatedInfo = { ...info, [name]: value };
+        setInfo(updatedInfo);
     };
-    // Validate form fields and set errors if any
-    const validateForm = () => {
-        let tempErrors = {};
-        if (info.name && !info.name.trim()) tempErrors.name = "Name is required";
-        if (info.email && !validator.isEmail(info.email)) tempErrors.email = "Invalid email address";
-        if (info.phone && !validator.isMobilePhone(info.phone)) tempErrors.phone = "Invalid phone number";
-        if (info.address && !info.address.trim()) tempErrors.address = "Address is required";
-        if (info.link && !validator.isURL(info.link, { protocols: ['http', 'https'], require_protocol: true })) tempErrors.link = "Invalid URL";
-        setErrors(tempErrors);
-        return Object.keys(tempErrors).length === 0;
-    };
-    // Handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            console.log(info);
-            setSubmittedInfo(info);
-            setIsEditing(false);
-        }
-    };
-    // When editing the information
-    const handleEdit = () => {
-        setIsEditing(true);
-    };
-    // Component JSX
+
     return (
-        <div>
-            <h3>Personal Details</h3>
-            {isEditing ? (
-                <form id='generaInfoForm' onSubmit={handleSubmit}>
-                    {/* Name input field */}
-                    <div className='form-fields'>
-                    <div className='form-field'>
-                        <label className='label' htmlFor="name">Full Name:</label>
-                        <input
-                            className="input"
-                            type="text"
-                            name="name"
-                            id="name"
-                            value={info.name}
-                            onChange={handleChange}
-                        />
-                        {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
-                    </div>
-                    {/* Email input field */}
-                    <div className='form-field'>
-                        <label className='label' htmlFor="email">Email:</label>
-                        <input
-                            className="input"
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={info.email}
-                            onChange={handleChange}
-                        />
-                        {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
-                    </div>
-                    {/* Phone number input field */}
-                    <div className='form-field'>
-                        <label className='label' htmlFor="phone">Phone Number:</label>
-                        <input
-                            className="input"
-                            type="tel"
-                            name="phone"
-                            id="phone"
-                            value={info.phone}
-                            onChange={handleChange}
-                        />
-                        {errors.phone && <p style={{ color: 'red' }}>{errors.phone}</p>}
-                    </div>
-                    {/* Address input field */}
-                    <div className='form-field'>
-                        <label className='label' htmlFor="address">Location:</label>
-                        <input
-                            className="input"
-                            type="text"
-                            name="address"
-                            id="address"
-                            value={info.address}
-                            onChange={handleChange}
-                        />
-                        {errors.address && <p style={{ color: 'red' }}>{errors.address}</p>}
-                    </div>
-                    {/* Link input field */}
-                    <div className='form-field'>
-                        <label className='label' htmlFor="link">Link:</label>
-                        <input
-                            className="input"
-                            type="text"
-                            name="link"
-                            id="link"
-                            value={info.link}
-                            onChange={handleChange}
-                        />
-                        {errors.link && <p style={{ color: 'red' }}>{errors.link}</p>}
-                    </div>
-                    </div>
-                        <Button id='generaInfoForm' secondary type="submit">Save</Button>
-                </form>
-            ) : (
-                /* Display submitted information */
-                <div>
-                    <p>Name: {submittedInfo.name}</p>
-                    <p>Email: {submittedInfo.email}</p>
-                    <p>Phone: {submittedInfo.phone}</p>
-                    <p>Address: {submittedInfo.address}</p>
-                    <p>Link:
-                        <a href={submittedInfo.link.startsWith('http') ?
-                            submittedInfo.link : `http://${submittedInfo.link}`}
-                            target="_blank" rel="noopener noreferrer">
-                            {submittedInfo.link}
-                        </a>
-                    </p>
-                    {/* <button onClick={handleEdit}>Edit</button> */}
-
-                    <Button primary onClick={handleEdit}>Edit</Button>
-                </div>
-            )}
-        </div>
+        <Box sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}>
+            <h1>General Information</h1>
+            <TextField
+                name="firstName"
+                label="First Name"
+                value={info.firstName}
+                onChange={handleInputChange}
+                variant="outlined"
+            />
+            <TextField
+                name="lastName"
+                label="Last Name"
+                value={info.lastName}
+                onChange={handleInputChange}
+                variant="outlined"
+            />
+            <TextField
+                label="Email"
+                type="email"
+                name="email"
+                value={info.email}
+                onChange={handleInputChange}
+                variant="outlined"
+            />
+            <TextField
+                label="Phone Number"
+                type="tel"
+                name="phoneNumber"
+                value={info.phoneNumber}
+                onChange={handleInputChange}
+                variant="outlined"
+            />
+            <TextField
+                label="Address"
+                name="address"
+                value={info.address}
+                onChange={handleInputChange}
+                variant="outlined"
+            />
+            <TextField
+                label="Website"
+                name="portfolioLink"
+                value={info.portfolioLink}
+                type="url"
+                onChange={handleInputChange}
+                variant="outlined"
+            />
+        </Box>
     );
 }
